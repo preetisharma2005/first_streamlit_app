e9f687f6d7f9b43e6c6bc0980c36e9ba4b4c44dd
Preeti Sharma <93264428+preetisharma2005@users.noreply.github.com>
2022-11-01 18:23:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title ('Happy to see my first app.')
+streamlit.title ('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
